854e840c94dd8ff1f51d5d2f743cd5fa17b0a906
Jonathan Mah <jon@jonmah.com>
2021-02-17 11:39:43 -0800

    Actually add layout components and global theme

---
+++ src/components/Header/Header.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { Container } from './Header.styled'
+
+const Header = () => <Container>Header</Container>
+
+export default Header
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { Footer, Header } from '../../components'
+
+const Layout = () => (
+  <>
+    <Header />
+    <Footer />
+  </>
+)
+
+export default Layout
